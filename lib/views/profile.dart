acd7d087fc6d69532fa6a49f6e0719ed252bc0d4
Enkiiii <113025916+Enkiiii@users.noreply.github.com>
2023-03-13 15:08:58 +0100

    Update Profile view test

--- lib/views/profile.dart
+++ lib/views/profile.dart
@@ -1,3 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_redditech_epitech/services/profile_service.dart';
+
+import '../models/profiles_model.dart';
+
 class Profile extends StatefulWidget {
   const Profile({super.key, required this.username});
 
@@ -8,14 +13,38 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
-  Profile? profile;
+  ProfilesModel? profile;
 
   getProfile() async {
-    final res = await 
+    final res = await ProfileService().getProfile();
+    setState(() {
+      profile = res;
+    });
+  }
+
+  @override
+  void initState() {
+    getProfile();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Profile"),
+      ),
+      body: profile == null
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : Column(
+              children: [
+                Text(profile!.username),
+                Image.network(profile!.picture),
+                Text(profile!.description),
+              ],
+            ),
+    );
   }
 }
