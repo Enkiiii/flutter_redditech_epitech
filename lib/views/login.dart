57a423ef0dd2767142dc654f5f57350e210421c0
Enkiiii <113025916+Enkiiii@users.noreply.github.com>
2023-03-13 17:07:59 +0100

    Profile service & view update

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -11,23 +11,25 @@ class LoginPage extends StatelessWidget {
     size.width;
     size.height;
     return Scaffold(
-        appBar: AppBar(title: const Text("login")),
-        body: Center(
-          child: ElevatedButton(
-              onPressed: () async {
-                await AuthService().login() ;
-                // Une fois qu'on a le token,
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const HomePage(),
-                  ),
-                );
-              },
-              child: SizedBox(
-                width: size.width * 0.7,
-                child: const Text("login"),
-              )),
-        ));
+      appBar: AppBar(title: const Text("login")),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () async {
+            await AuthService().login();
+            // Une fois qu'on a le token,
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const HomePage(),
+              ),
+            );
+          },
+          child: SizedBox(
+            width: size.width * 0.7,
+            child: const Text("login"),
+          ),
+        ),
+      ),
+    );
   }
 }
